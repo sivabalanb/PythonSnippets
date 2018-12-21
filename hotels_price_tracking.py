ff9dcc5adb5c8e4a75afb8b78c7e5aabe3c358b1
Sivabalan <sivabalanb.92@gmail.com>
2018-12-21 17:48:20 +0530

    Writing only the calues
    + Still does not work as expected

--- hotels_price_tracking.py
+++ hotels_price_tracking.py
@@ -1,11 +1,10 @@
-urls = ["https://www.google.com/travel/hotels/Pa%20Tong/place/15124167736024278125?ap=MAFaUgoFCPAuEAAiA0lOUioWCgcI4w8QARgPEgcI4w8QARgQGAEoADgEOAVyAggCmgEIEgZQYXRvbmeiARMKCS9tLzA0NHRkNRIGUGF0b25nkgECIAFwAA&g2lb=4181926%2C4208993%2C4209588%2C4223281%2C4225904%2C4227717%2C4207631%2C4215556%2C4220469%2C4226109&hl=en&gl=in&un=0&q=4%20star%20hotels%20patong&rp=OAFAAA&ictx=1&ved=0CKYBEKjgAigLahcKEwig5Oumn7DfAhUAAAAAHQAAAAAQEA&hrf=CgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABOAQ4BXICCAKaAQgSBlBhdG9uZ6IBEwoJL20vMDQ0dGQ1EgZQYXRvbmeSAQIgAQ&tcfs=Ei0KCS9tLzA0NHRkNRIGUGF0b25nGhgKCjIwMTktMDMtMDkSCjIwMTktMDMtMTIYAiIYCgoyMDE5LTAzLTA5EgoyMDE5LTAzLTEyUgJIBA"
-"https://www.google.com/travel/hotels/Pa%20Tong/place/7507267568307879052?ap=MAFaUAoFCPAuEAAiA0lOUioWCgcI4w8QAxgJEgcI4w8QAxgMGAMgATgFcgIIAZoBCBIGUGF0b25nogETCgkvbS8wNDR0ZDUSBlBhdG9uZ5IBAiABcAA&g2lb=4181926%2C4208993%2C4209588%2C4223281%2C4225904%2C4227717%2C4207631%2C4215556%2C4220469%2C4226109&hl=en&gl=in&un=0&q=4%20star%20hotels%20patong&rp=OAFAAA&ictx=1&ved=0CCgQqOACKAJqFwoTCKDk66afsN8CFQAAAAAdAAAAABBS&hrf=CgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABOAVoA3ICCAGaAQgSBlBhdG9uZ6IBEwoJL20vMDQ0dGQ1EgZQYXRvbmeSAQIgAQ&tcfs=Ei0KCS9tLzA0NHRkNRIGUGF0b25nGhgKCjIwMTktMDMtMDkSCjIwMTktMDMtMTIYASIYCgoyMDE5LTAzLTA5EgoyMDE5LTAzLTEyUgJIBQ"]
-'''
+urls = ["https://www.google.com/travel/hotels/Pa%20Tong/place/15124167736024278125?ap=MAFaUgoFCPAuEAAiA0lOUioWCgcI4w8QARgPEgcI4w8QARgQGAEoADgEOAVyAggCmgEIEgZQYXRvbmeiARMKCS9tLzA0NHRkNRIGUGF0b25nkgECIAFwAA&g2lb=4181926%2C4208993%2C4209588%2C4223281%2C4225904%2C4227717%2C4207631%2C4215556%2C4220469%2C4226109&hl=en&gl=in&un=0&q=4%20star%20hotels%20patong&rp=OAFAAA&ictx=1&ved=0CKYBEKjgAigLahcKEwig5Oumn7DfAhUAAAAAHQAAAAAQEA&hrf=CgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABOAQ4BXICCAKaAQgSBlBhdG9uZ6IBEwoJL20vMDQ0dGQ1EgZQYXRvbmeSAQIgAQ&tcfs=Ei0KCS9tLzA0NHRkNRIGUGF0b25nGhgKCjIwMTktMDMtMDkSCjIwMTktMDMtMTIYAiIYCgoyMDE5LTAzLTA5EgoyMDE5LTAzLTEyUgJIBA",
+"https://www.google.com/travel/hotels/Pa%20Tong/place/7507267568307879052?ap=MAFaUAoFCPAuEAAiA0lOUioWCgcI4w8QAxgJEgcI4w8QAxgMGAMgATgFcgIIAZoBCBIGUGF0b25nogETCgkvbS8wNDR0ZDUSBlBhdG9uZ5IBAiABcAA&g2lb=4181926%2C4208993%2C4209588%2C4223281%2C4225904%2C4227717%2C4207631%2C4215556%2C4220469%2C4226109&hl=en&gl=in&un=0&q=4%20star%20hotels%20patong&rp=OAFAAA&ictx=1&ved=0CCgQqOACKAJqFwoTCKDk66afsN8CFQAAAAAdAAAAABBS&hrf=CgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABOAVoA3ICCAGaAQgSBlBhdG9uZ6IBEwoJL20vMDQ0dGQ1EgZQYXRvbmeSAQIgAQ&tcfs=Ei0KCS9tLzA0NHRkNRIGUGF0b25nGhgKCjIwMTktMDMtMDkSCjIwMTktMDMtMTIYASIYCgoyMDE5LTAzLTA5EgoyMDE5LTAzLTEyUgJIBQ",
 "https://www.google.com/travel/hotels/Pa%20Tong/place/12760924574243379521?ap=MAFaUAoFCPAuEAAiA0lOUioWCgcI4w8QAxgJEgcI4w8QAxgMGAMgATgFcgIIAZoBCBIGUGF0b25nogETCgkvbS8wNDR0ZDUSBlBhdG9uZ5IBAiABcAA&g2lb=4181926%2C4208993%2C4209588%2C4223281%2C4225904%2C4227717%2C4207631%2C4215556%2C4220469%2C4226109&hl=en&gl=in&un=0&q=4%20star%20hotels%20patong&rp=OAFAAA&ictx=1&ved=0CJgBEKjgAigKahcKEwig5Oumn7DfAhUAAAAAHQAAAAAQUg&hrf=CgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABOAVoA3ICCAGaAQgSBlBhdG9uZ6IBEwoJL20vMDQ0dGQ1EgZQYXRvbmeSAQIgAQ&tcfs=Ei0KCS9tLzA0NHRkNRIGUGF0b25nGhgKCjIwMTktMDMtMDkSCjIwMTktMDMtMTIYASIYCgoyMDE5LTAzLTA5EgoyMDE5LTAzLTEyUgJIBQ",
 "https://www.google.com/travel/hotels/Pa%20Tong/place/997620782875815276?ap=EgNDQXcwA1pQCgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABaANyAggBmgEIEgZQYXRvbmeiARMKCS9tLzA0NHRkNRIGUGF0b25nkgECIAFwAA&g2lb=4181926%2C4208993%2C4209588%2C4223281%2C4225904%2C4227717%2C4207631%2C4215556%2C4220469%2C4226109&hl=en&gl=in&un=0&q=4%20star%20hotels%20patong&rp=OAFAAA&ictx=1&ved=0CJgBEKjgAigKahcKEwiA6qKyq7DfAhUAAAAAHQAAAAAQIw&hrf=CgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABOARoA3ICCAGaAQgSBlBhdG9uZ6IBEwoJL20vMDQ0dGQ1EgZQYXRvbmeSAQIgAQ&tcfs=Ei0KCS9tLzA0NHRkNRIGUGF0b25nGhgKCjIwMTktMDMtMDkSCjIwMTktMDMtMTIYASIYCgoyMDE5LTAzLTA5EgoyMDE5LTAzLTEyUgJIBA",
 "https://www.google.com/travel/hotels/Pa%20Tong/place/8443959769382945276?ap=EgNDREEwA1pQCgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABaANyAggBmgEIEgZQYXRvbmeiARMKCS9tLzA0NHRkNRIGUGF0b25nkgECIAFwAA&g2lb=4181926%2C4208993%2C4209588%2C4223281%2C4225904%2C4227717%2C4207631%2C4215556%2C4220469%2C4226109&hl=en&gl=in&un=0&q=4%20star%20hotels%20patong&rp=OAFAAA&ictx=1&ved=0CJgBEKjgAigKahcKEwiA6qKyq7DfAhUAAAAAHQAAAAAQSw&hrf=CgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABOARoA3ICCAGaAQgSBlBhdG9uZ6IBEwoJL20vMDQ0dGQ1EgZQYXRvbmeSAQIgAQ&tcfs=Ei0KCS9tLzA0NHRkNRIGUGF0b25nGhgKCjIwMTktMDMtMDkSCjIwMTktMDMtMTIYASIYCgoyMDE5LTAzLTA5EgoyMDE5LTAzLTEyUgJIBA",
 "https://www.google.com/travel/hotels/Pa%20Tong/place/11616903417879953370?ap=EgNDREEwA1pQCgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABaANyAggBmgEIEgZQYXRvbmeiARMKCS9tLzA0NHRkNRIGUGF0b25nkgECIAFwAA&g2lb=4181926%2C4208993%2C4209588%2C4223281%2C4225904%2C4227717%2C4207631%2C4215556%2C4220469%2C4226109&hl=en&gl=in&un=0&q=4%20star%20hotels%20patong&rp=OAFAAA&ictx=1&ved=0CIoBEKjgAigJahcKEwiA6qKyq7DfAhUAAAAAHQAAAAAQSw&hrf=CgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABOARoA3ICCAGaAQgSBlBhdG9uZ6IBEwoJL20vMDQ0dGQ1EgZQYXRvbmeSAQIgAQ&tcfs=Ei0KCS9tLzA0NHRkNRIGUGF0b25nGhgKCjIwMTktMDMtMDkSCjIwMTktMDMtMTIYASIYCgoyMDE5LTAzLTA5EgoyMDE5LTAzLTEyUgJIBA"]
-'''
+
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -18,6 +17,7 @@ try:
 		print(old_prices)
 
 except Exception as e:
+	old_prices = {}
 	print("Failed to read the csv",e)
 
 for url in urls:
@@ -35,26 +35,39 @@ for url in urls:
 	tags = soup.findAll("span", {"class": "eDCB7b"})
 	price = []
 	for tag in tags:
-	    price_t = tag.text.encode("utf-8")[5:]
-	    price_t = int(price_t.replace(",", ""))
-	    price.append(price_t)
+		price_t = tag.text.encode("utf-8")[5:]
+		price_t = price_t.replace(",", "")
+		try:
+			price_t = int(price_t)
+		except Exception as e:
+			print("Exception while converting the price to integer",e)
+		
+		if isinstance(price_t, int):
+			price.append(price_t)
 
 	print(price)
-	#print("Minimum price:\t",min(price))
-	#print("Maximum price:\t",max(price))
+	print("Minimum price:\t",min(price))
+	print("Maximum price:\t",max(price))
 	my_hotel_dict = {}
+	print("old_prices[hotel_name]",old_prices[hotel_name])
 	try:
 		if old_prices[hotel_name] > min(price):
 			price_change = old_prices[hotel_name] - min(price)
 			my_hotel_dict[hotel_name] = [min(price), price_change]
 	except Exception as e:
 		print("Exception while calculating price_change:\t",e)
-		price_change = old_prices[hotel_name] - min(price)
+		if hotel_name in old_prices:
+			price_change = int(old_prices[hotel_name][1:5]) - min(price)
+			print("Price change:\t",price_change)
+		else:
+			price_change = 0
+
 		my_hotel_dict[hotel_name] = [min(price), price_change]
 
 	with open('price_tracker.csv', 'a') as csv_file:
 		writer = csv.writer(csv_file)
 		for key, value in my_hotel_dict.items():
-		   writer.writerow([key, value])
+			#value_t = [x.strip() for x in value.split(',')]
+			writer.writerow([key, value[0], value[1]])
 
 	time.sleep(60)
