f3954bad3e1af8bc0f342a92b50b355272c096e1
Sivabalan <sivabalanb.92@gmail.com>
2018-12-21 12:01:04 +0530

    Simple dict to store hotel name and its lowest price

---
+++ hotels_price_tracking.py
@@ -0,0 +1,30 @@
+url="https://www.google.com/travel/hotels/Pa%20Tong/place/15124167736024278125?ap=MAFaUgoFCPAuEAAiA0lOUioWCgcI4w8QARgPEgcI4w8QARgQGAEoADgEOAVyAggCmgEIEgZQYXRvbmeiARMKCS9tLzA0NHRkNRIGUGF0b25nkgECIAFwAA&g2lb=4181926%2C4208993%2C4209588%2C4223281%2C4225904%2C4227717%2C4207631%2C4215556%2C4220469%2C4226109&hl=en&gl=in&un=0&q=4%20star%20hotels%20patong&rp=OAFAAA&ictx=1&ved=0CKYBEKjgAigLahcKEwig5Oumn7DfAhUAAAAAHQAAAAAQEA&hrf=CgUI8C4QACIDSU5SKhYKBwjjDxADGAkSBwjjDxADGAwYAyABOAQ4BXICCAKaAQgSBlBhdG9uZ6IBEwoJL20vMDQ0dGQ1EgZQYXRvbmeSAQIgAQ&tcfs=Ei0KCS9tLzA0NHRkNRIGUGF0b25nGhgKCjIwMTktMDMtMDkSCjIwMTktMDMtMTIYAiIYCgoyMDE5LTAzLTA5EgoyMDE5LTAzLTEyUgJIBA"
+
+import requests
+from bs4 import BeautifulSoup
+headers = requests.utils.default_headers()
+headers.update({
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+})
+
+Link = requests.get(url, headers=headers)
+soup =BeautifulSoup(Link.content,"lxml")
+
+hotel_name =  soup.find('title')
+hotel_name = hotel_name.text
+#print("Name of the hotel:\t",hotel_name)
+tags = soup.findAll("span", {"class": "eDCB7b"})
+price = []
+for tag in tags:
+    price_t = tag.text.encode("utf-8")[5:]
+    price_t = int(price_t.replace(",", ""))
+    price.append(price_t)
+
+#print(price)
+#print("Minimum price:\t",min(price))
+#print("Maximum price:\t",max(price))
+my_hotel_dict = {}
+
+my_hotel_dict[hotel_name] = [min(price),max(price)]
+
+print("Hotel stored in dict:\t", my_hotel_dict)
