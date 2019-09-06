e17ba433a46b303e8f9cfcad2ad81ba788714913
sivabalan <sivabalanb.92@gmail.com>
2019-09-06 10:35:39 +0530

    Datastructures
    Tuples as records

---
+++ datastructures.py
@@ -0,0 +1,8 @@
+#Tuples used as records
+
+lax_coordinates = (33.9425, -118.408056)
+city, year, pop, chg, area = ('Tokyo', 2003, 32450, 0.66, 8014)
+traveler_ids = [('USA', '31195855'), ('BRA', 'CE342567'),('ESP', 'XDA205856')]
+
+for passport in sorted(traveler_ids):
+	print('%s/%s' % passport)
