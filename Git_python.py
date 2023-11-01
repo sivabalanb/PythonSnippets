62b1c38b09be56099c744abdd39aa3d538296719
Sivabalan <sivabalanb.92@gmail.com>
2023-11-01 15:15:04 +0000

    test

--- Git_python.py
+++ Git_python.py
@@ -1,22 +1,28 @@
-import os
-import datetime
-import git
+import subprocess
+import time
 
-repo = git.Repo(os.getcwd())
+# Replace with your GitHub username and repository name
+github_username = "sivabalanb"
+repository_name = " PythonSnippets"
 
-master = repo.head.reference
+# Number of commits you want to make
+num_commits = 2
 
-print(master.name)
+# Content for the commits
+commit_message = "Automated commit test."
 
-print(master.commit.message)
+for i in range(num_commits):
+    # Create or modify a file with a timestamp
+    timestamp = time.time()
+    filename = f"commit_{i}.txt"
+    with open(filename, "w") as file:
+        file.write(f"Commit {i} at {timestamp}")
 
-print(datetime.datetime.fromtimestamp(master.commit.committed_date))
+    # Stage and commit the changes
+    subprocess.run(["git", "add", filename])
+    subprocess.run(["git", "commit", "-m", commit_message])
 
-#repo = Repo(path)
-from git import Repo, Commit
+    # Push the changes to the repository
+    subprocess.run(["git", "push", "origin", "main"])  # Replace 'main' with your branch name
 
-repo = git.Repo(os.getcwd())
-
-for commit in repo.iter_commits():
-    print("Author: ", commit.author)
-    print("Summary: ", commit.message)
+print(f"{num_commits} commits made to the repository.")
